ff132ca10e97c0bab15f4f0157c082672db344c5
Vinicius01br <viniciuslima1809@hotmail.com>
2024-04-16 17:19:52 -0300

    exercicio js arrays

---
+++ js/02-arrays.js
@@ -0,0 +1,14 @@
+`use strict`;
+console.log("Arrays!");
+//Declarando um arrays de alunos
+const alunos = ["William", "Vinicius", "Marli", "Duda", "Gabriel",]
+//Exibindo a estrutura do array (somente no console)
+console.log(alunos)
+//acessando individualmente alguns alunos através do indice
+console.log(alunos[1]);//Vincius
+console.log(alunos[3]);//Duda
+console.log(alunos[0]);//William
+
+const hobby = ["Videogame","Música","Tecnologia", "Filmes", "Comida", "Animes", "Séries"]
+
+console.log(`Sou uma pessoa que gosta muito de ${hobby[1]}, e música e apaixonado por tecnologia, nas horas vagas gosto de assistir filmes e series e animes com um pouco de ${hobby[4]}`);
