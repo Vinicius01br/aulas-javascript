49979590899ae888688cabdfad0e4d2043ebb20a
Vinicius01br <viniciuslima1809@hotmail.com>
2024-04-18 14:59:36 -0300

    Exercicio 18/04

---
+++ exercicio01.js
@@ -0,0 +1,13 @@
+"use strict";
+let nota1 = 10;
+let nota2 = 1;
+let total = nota1+nota2; 
+let media = total/2;
+console.log(media);
+if(media >= 7){
+    console.log("aprovado");
+} else if(media >= 5){
+    console.log("recuperação");
+}else{
+    console.log("recuperação");
+}
